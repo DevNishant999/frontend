0a4645ef6993617883d9d642ea81a51d0aab73b5
Nishant9091 <nishantsharma7428@gmail.com>
2025-07-29 02:32:18 +0530

    perofrmance optimize

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,71 @@
-import React from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-import Home from "./pages/home/Home.jsx";
-import Service from "./pages/service/Service.jsx"; // Create this component
-import Results from "./pages/results/Results.jsx";
-import Cost from "./pages/cost/Cost.jsx";
 import Booknow from "./components/Booknow.jsx";
-import AboutUs from "./pages/about/AboutUs.jsx";
-import ContactUs from "./pages/contactus/ContactUs.jsx";
-import Blogs from "./pages/blogs/Blogs.jsx";
-import BlogDetails from "./pages/blog-details/BlogDetails.jsx";
-import OurClinic from "./pages/our-clinic/OurClinic.jsx";
-import Thankyou from "./pages/Thankyou.jsx";
-import FourOFour from "./pages/FourOFour.jsx";
-import PaymentTC from "./pages/PaymentTC.jsx";
-import PrivacyPolicy from "./pages/PrivacyPolicy.jsx";
-import Faqs from "./pages/Faqs.jsx";
-import MedicalTourism from "./pages/medical-tourism/MedicalTourism.jsx";
-import Country from "./pages/country/Country.jsx";
-import City from "./pages/city/City.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+// Lazy loaded pages
+const Home = lazy(() => import("./pages/home/Home.jsx"));
+const Service = lazy(() => import("./pages/service/Service.jsx"));
+const Results = lazy(() => import("./pages/results/Results.jsx"));
+const Cost = lazy(() => import("./pages/cost/Cost.jsx"));
+const AboutUs = lazy(() => import("./pages/about/AboutUs.jsx"));
+const ContactUs = lazy(() => import("./pages/contactus/ContactUs.jsx"));
+const Blogs = lazy(() => import("./pages/blogs/Blogs.jsx"));
+const BlogDetails = lazy(() => import("./pages/blog-details/BlogDetails.jsx"));
+const OurClinic = lazy(() => import("./pages/our-clinic/OurClinic.jsx"));
+const Thankyou = lazy(() => import("./pages/Thankyou.jsx"));
+const FourOFour = lazy(() => import("./pages/FourOFour.jsx"));
+const PaymentTC = lazy(() => import("./pages/PaymentTC.jsx"));
+const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy.jsx"));
+const Faqs = lazy(() => import("./pages/Faqs.jsx"));
+const MedicalTourism = lazy(() =>
+  import("./pages/medical-tourism/MedicalTourism.jsx")
+);
+const Country = lazy(() => import("./pages/country/Country.jsx"));
+const City = lazy(() => import("./pages/city/City.jsx"));
 
 const App = () => {
+  useEffect(() => {
+    const images = document.querySelectorAll("img");
+    images.forEach((img) => {
+      if (!img.hasAttribute("loading")) {
+        img.setAttribute("loading", "lazy");
+      }
+      if (!img.hasAttribute("decoding")) {
+        img.setAttribute("decoding", "async");
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <Header />
       <Booknow />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/service/:slug" element={<Service />} />
-        <Route path="/country/:slug" element={<Country />} />
-        <Route path="/city/:slug" element={<City />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/cost/:slug" element={<Cost />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blog/:slug" element={<BlogDetails />} />
-        <Route path="/guide/:slug" element={<BlogDetails />} />
-        <Route path="/our-clinic" element={<OurClinic />} />
-        <Route path="/thankyou" element={<Thankyou />} />
-        <Route path="*" element={<FourOFour />} />
-        <Route path="/payment-tc" element={<PaymentTC />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/faqs" element={<Faqs />} />
-        <Route path="/medical-tourism" element={<MedicalTourism />} />
-      </Routes>
+      <Suspense fallback={<div className="loading">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/service/:slug" element={<Service />} />
+          <Route path="/country/:slug" element={<Country />} />
+          <Route path="/city/:slug" element={<City />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/cost/:slug" element={<Cost />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blog/:slug" element={<BlogDetails />} />
+          <Route path="/guide/:slug" element={<BlogDetails />} />
+          <Route path="/our-clinic" element={<OurClinic />} />
+          <Route path="/thankyou" element={<Thankyou />} />
+          <Route path="/payment-tc" element={<PaymentTC />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/faqs" element={<Faqs />} />
+          <Route path="/medical-tourism" element={<MedicalTourism />} />
+          <Route path="*" element={<FourOFour />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./Home.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import {
   ReactCompareSlider,
   ReactCompareSliderImage,
